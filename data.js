b5efc545fcf987b0f0d377fc288d3c63475f5dc0
Simon Li <simonltk@gmail.com>
2026-01-31 22:24:42 +0000

    init

---
+++ data.js
@@ -0,0 +1,54 @@
+const sets = {
+    set1: {
+        title: "1 - Opinions & Preferences",
+        cards: [
+            { es: "Prefiero", en: "I prefer" },
+            { es: "Me interesa", en: "I'm interested in" },
+            { es: "Me encanta", en: "I love" },
+            { es: "Me fascina", en: "I'm fascinated by" },
+            { es: "Me gusta mucho", en: "I really like" },
+            { es: "Me gusta bastante", en: "I quite like" },
+            { es: "Me gusta", en: "I like" },
+            { es: "Me da igual", en: "I don't mind" },
+            { es: "No me gusta", en: "I don't like" },
+            { es: "No me gusta nada", en: "I don't like at all" },
+            { es: "No soporto", en: "I can't stand" },
+            { es: "Odio", en: "I hate" },
+            { es: "Es", en: "He/she/it is" },
+            { es: "No es", en: "He/she/it isn't" },
+        ]
+    },
+    set2: {
+        title: "2 - Free Time Questions",
+        cards: [
+            { es: "¿Qué te gusta hacer en tu tiempo libre?", en: "What do you like to do in your free time?" },
+            { es: "¿Qué no te gusta hacer en tu tiempo libre?", en: "What do you not like to do in your free time?" },
+            { es: "¿Prefieres ir al cine o leer?", en: "Do you prefer to go to the cinema or read?" },
+            { es: "¿Qué deporte te gusta hacer? ¿Por qué?", en: "What sport do you like to do? Why?" },
+            { es: "¿Eres deportista?", en: "Are you sporty?" }
+        ]
+    },
+    set3: {
+        title: "3 - Frequency Phrases",
+        cards: [
+            { es: "Los lunes", en: "On Mondays" },
+            { es: "Los martes", en: "On Tuesdays" },
+            { es: "Los miércoles", en: "On Wednesdays" },
+            { es: "Los jueves", en: "On Thursdays" },
+            { es: "Los viernes", en: "On Fridays" },
+            { es: "Los lunes por la mañana", en: "On Monday mornings" },
+            { es: "Los lunes por la tarde", en: "On Monday afternoons" },
+            { es: "Todos los días", en: "Every day" },
+            { es: "Todos los lunes", en: "Every Monday" },
+            { es: "Cada día", en: "Every day" }
+        ]
+    }
+};
+
+const praises = [
+    "Increíble! You're a natural!",
+    "Fantastic work! You learned them all!",
+    "You're a Spanish Superstar! 🌟",
+    "High Five! You finished the set!",
+    "Magnífico! Your Spanish is growing!"
+];
